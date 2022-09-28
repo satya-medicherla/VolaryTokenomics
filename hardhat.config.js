93bc918825d6a4bb0154104d1b3799a37e287438
satya <98799086+satya-nest@users.noreply.github.com>
2022-09-28 16:09:57 +0530

    ft hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.17",
-};
+ require("@nomiclabs/hardhat-ethers");
+ require("@nomiclabs/hardhat-waffle");
+ require("solidity-coverage");
+ require("@nomiclabs/hardhat-web3");
+ require("@nomiclabs/hardhat-truffle5");
+ module.exports = {
+   solidity: "0.8.17",
+ };
