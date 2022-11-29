ddbd7b82c99dfed4473b659d1ebde06e1c6b3a66
satya <98799086+satya-nest@users.noreply.github.com>
2022-11-29 15:07:35 +0530

    ft coverage

--- test/test.js
+++ test/test.js
@@ -15,11 +15,13 @@ describe('test volary contract deployment',(accounts) => {
     before(async function () {
         accounts = await web3.eth.getAccounts();
         volary = await tokenContract.new();
-        stacking = await stackingContract.new();
+        stacking = await stackingContract.new(volary.address);
         rewardPool = await rewardsContract.new(volary.address,stacking.address,
             volary.address,volary.address,accounts[0]);
-            await stacking.addRewardPoolAddress(rewardPool.address);
-            await stacking.addTokenContract(volary.address);
+            await truffleAssert.reverts(stacking.createStake("1900",0))
+            await truffleAssert.reverts(stacking.addRewardPoolAddress(rewardPool.address,{from : accounts[8]}))
+            await stacking.addRewardPoolAddress(rewardPool.address)
+            await truffleAssert.reverts(stacking.createStake("1900",0))
             await volary.transfer(rewardPool.address,"1000000000000000000000");
             await volary.transfer(accounts[1],"200000000000000000000");
             await volary.transfer(accounts[2],"100000000000000000000");
@@ -51,9 +53,11 @@ describe('test volary contract deployment',(accounts) => {
         await stacking.createStake("100000000000000000000",13305900,{from : accounts[2]});
         assert.equal(await stacking.isStakeHolder(accounts[2]),true);
         assert.equal(await stacking.getStakeAmount(1),"100000000000000000000");
-        assert.equal(await rewardPool.CURRENT_EPOCH(),0)
+        assert.equal(await rewardPool.CURRENT_EPOCH(),0);
+        await truffleAssert.reverts(rewardPool.startPool({from : accounts[1]}))
         await rewardPool.startPool();
         assert.equal(await rewardPool.CURRENT_EPOCH(),1);
+        
         await ethers.provider.send('evm_increaseTime', [epochTime-2]);
         await ethers.provider.send('evm_mine');
         assert.equal(await stacking.getstakesTillEpoch(1),2)
@@ -72,6 +76,7 @@ describe('test volary contract deployment',(accounts) => {
         await rewardPool.calculateRewardWeight(1,1300000,1500000,1000000,2432670);
         await rewardPool.rewardOfStake(0);
         await rewardPool.rewardOfStake(1);
+        await truffleAssert.reverts(rewardPool.finishEpoch({from : accounts[2]}));
         await rewardPool.finishEpoch();
         /**
          *  accumalted rewards after epoch is finished
@@ -138,6 +143,7 @@ describe('test volary contract deployment',(accounts) => {
         
         truffleAssert.reverts(rewardPool.claimRewards(0,"927348539605053374",{from:accounts[0]}));
         truffleAssert.reverts(rewardPool.claimRewards(1,"927348539605053375",{from:accounts[0]}));
+        truffleAssert.reverts(rewardPool.claimRewards(0,"927348539605053375",{from:accounts[1]}));
 
         await rewardPool.claimRewards(0,"927348539605053374",{from:accounts[1]})
 
@@ -154,10 +160,13 @@ describe('test volary contract deployment',(accounts) => {
         await truffleAssert.reverts(rewardPool.finishEpoch());
         await ethers.provider.send('evm_increaseTime', [epochTime]);
         await ethers.provider.send('evm_mine');
+        await truffleAssert.reverts(rewardPool.rewardOfStake(0));
         await rewardPool.calculateRewardWeight(0,1100000,1200000,1000000,1000000);
         await rewardPool.calculateRewardWeight(1,1300000,1500000,1000000,2432670);
         await rewardPool.rewardOfStake(0);
+        await truffleAssert.reverts(rewardPool.rewardOfStake(0));
         await rewardPool.rewardOfStake(1);
+        await truffleAssert.reverts(rewardPool.rewardOfStake(9));
         await rewardPool.finishEpoch();
         assert.equal(await rewardPool.CURRENT_EPOCH(),4);
 
@@ -196,7 +205,38 @@ describe('test volary contract deployment',(accounts) => {
         assert.equal(balance.toString(),"927348539605053374")
         balance=await rewardPool.CLAIMED_REWARDS(1)
         assert.equal(balance.toString(),"0")
+        await truffleAssert.reverts(rewardPool.startPool());
+        balance= await stacking.getStartTimeOfStake(0);
+        assert.equal(await stacking.isDurationBound(1),true);
+        balance = await stacking.getStakeAmount(0)
+        assert.equal(balance.toString(),"200000000000000000000")
+        balance = await stacking.getStakeAmount(1)
+        assert.equal(balance.toString(),"50000000000000000000")
+        balance = await stacking.getDuration(1)
+        assert.equal(balance.toString(),"13305900")
+        assert.equal(await stacking.getStackerLevel(accounts[0]),"none");
+        await volary.transfer(accounts[1],"1667000000000000000000000");
+        await volary.approve(stacking.address,"1667000000000000000000000",{from : accounts[1]})
+        await stacking.createStake("1667000000000000000000",0,{from : accounts[1]});
+        assert.equal(await stacking.getStackerLevel(accounts[1]),"sliver");
+        await stacking.createStake("16670000000000000000000",0,{from : accounts[1]});
+        assert.equal(await stacking.getStackerLevel(accounts[1]),"gold");
+        await stacking.createStake("166700000000000000000000",0,{from : accounts[1]});
+        assert.equal(await stacking.getStackerLevel(accounts[1]),"diamond");
+        await volary.transfer(accounts[1],"1667000000000000000000000");
+        await volary.approve(stacking.address,"1667000000000000000000000",{from : accounts[1]})
+        await stacking.createStake("1667000000000000000000000",0,{from : accounts[1]});
+        assert.equal(await stacking.getStackerLevel(accounts[1]),"platinum");
         
+        assert.equal(await stacking.getTotalNumberOfStakes(),6);
+        assert.equal(await stacking.isDurationBound(6),false);
+        assert.equal(await stacking.stakedPeriod(16),false);
+        balance =await stacking.getStakeAmount(1);
+        assert.equal(balance.toString(),"50000000000000000000");
+        
+
+        
+
     })
 
 })
